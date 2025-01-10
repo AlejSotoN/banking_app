55a253bbdead3b9208a575e0a6803d4a9764ea02
alejandro <alejandrosotonmd@gmail.com>
2025-01-10 09:43:23 -0400

    implement initial file and folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ 
-  subsets: ["latin"], 
-  variable: '--font-inter'})
+const inter = Inter({
+  subsets: ["latin"],
+  variable: '--font-inter'
+});
 
-  const ibmPlexSerif = IBM_Plex_Serif({
+const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-ibm-plex-serf'
-})
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
   title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }

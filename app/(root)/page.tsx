0d3bfd375bb2eb81b1ea9066d4b36a20349b5848
alejandro <alejandrosotonmd@gmail.com>
2025-01-10 11:15:01 -0400

    implement initial Headerbox with chart and current balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import HeaderBox from "@/components/ui/HeaderBox"
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn = { firstName: 'Alejandro'}
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and trasanctions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1234.56}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
